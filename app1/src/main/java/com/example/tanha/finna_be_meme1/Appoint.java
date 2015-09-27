46d35d9b0b5f91e7cae4036d7cb622e8f27f9581
Tanha-Shah <tanhashah95@gmail.com>
2015-09-27 17:50:29 +0530

    view appointments

---
+++ app1/src/main/java/com/example/tanha/finna_be_meme1/Appoint.java
@@ -0,0 +1,15 @@
+package com.example.tanha.finna_be_meme1;
+
+/**
+ * Created by Tanha on 9/27/2015.
+ */
+public class Appoint {
+        String id;
+        String name;
+        public Appoint(String id,String name){
+            this.id=id;
+            this.name=name;
+        }
+    }
+
+
